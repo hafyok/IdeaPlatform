8ca5d5ad4e4aaa2149a2d783620f5da3f9107ad3
Haggai Khafizov <hafyok5777@gmail.com>
2024-09-05 17:00:37 +0300

    add Scaffold and main screen

--- app/src/main/java/com/example/ideaplatform/MainActivity.kt
+++ app/src/main/java/com/example/ideaplatform/MainActivity.kt
@@ -4,15 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ideaplatform.Data.ItemEntity
-import com.example.ideaplatform.Data.ItemRoomDatabase
+import com.example.ideaplatform.Presentation.MainScreen
 import com.example.ideaplatform.ui.theme.IdeaPlatformTheme
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,31 +14,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IdeaPlatformTheme {
-
-
+                MainScreen()
             }
         }
-        GlobalScope.launch {
-            this@MainActivity.let {
-                val item = ItemEntity(id = 0, name = "TEst", time = 151515, tags = "tag1", amount = 1500)
-                ItemRoomDatabase(it).itemDao().insert(item)
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IdeaPlatformTheme {
-        Greeting("Android")
+//        GlobalScope.launch {
+//            this@MainActivity.let {
+//                val item = ItemEntity(id = 0, name = "TEst", time = 151515, tags = "tag1", amount = 1500)
+//                ItemRoomDatabase(it).itemDao().insert(item)
+//            }
+//        }
     }
 }
---
+++ app/src/main/java/com/example/ideaplatform/Presentation/MainScreen.kt
@@ -0,0 +1,66 @@
+package com.example.ideaplatform.Presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ideaplatform.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    actionIconContentColor = Color.Black,
+                    navigationIconContentColor = Color.Black,
+                    scrolledContainerColor = Color.Black
+                ),
+                title = {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(id = R.string.list_items),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button  times.
+                """.trimIndent(),
+            )
+        }
+
+    }
+
+}
